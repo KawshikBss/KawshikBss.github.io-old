e433ae4c4d03516114394d36bfdbfa31bcc12497
KawshikBss <kawshikbss@gmail.com>
2023-05-05 21:55:32 +0600

    added module styles

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,43 @@
+import Link from "next/link";
 import React from "react";
+import style from "./navbar.module.css";
 
 function Navbar() {
-    return <div className="navbar-container">Navbar</div>;
+    return (
+        <div
+            className={[style["navbar-container"], "hidden md:flex"].join(" ")}
+        >
+            <span className={style["navbar-logo"]}>
+                <Link href={"/"} className={style["bar-logo"]}>
+                    KawshikBss
+                </Link>
+            </span>
+            <ul className={style["navbar-items"]}>
+                <li className={style["navbar-item-link"]}>
+                    <Link href={"/"}>
+                        <span className={style["navbar-item-sym"]}>#</span>home
+                    </Link>
+                </li>
+                <li className={style["navbar-item-link"]}>
+                    <Link href={"/"}>
+                        <span className={style["navbar-item-sym"]}>#</span>about
+                        me
+                    </Link>
+                </li>
+                <li className={style["navbar-item-link"]}>
+                    <Link href={"/"}>
+                        <span className={style["navbar-item-sym"]}>#</span>works
+                    </Link>
+                </li>
+                <li className={style["navbar-item-link"]}>
+                    <Link href={"/"}>
+                        <span className={style["navbar-item-sym"]}>#</span>
+                        contact me
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 export default Navbar;
