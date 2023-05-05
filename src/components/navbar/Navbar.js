bbe64d5ffdd34b5e6ac282a504840707ac423fdd
KawshikBss <kawshikbss@gmail.com>
2023-05-05 22:19:57 +0600

    completed menu

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,35 +7,39 @@ function Navbar() {
         <div
             className={[style["navbar-container"], "hidden md:flex"].join(" ")}
         >
-            <span className={style["navbar-logo"]}>
-                <Link href={"/"} className={style["bar-logo"]}>
-                    KawshikBss
-                </Link>
-            </span>
-            <ul className={style["navbar-items"]}>
-                <li className={style["navbar-item-link"]}>
-                    <Link href={"/"}>
-                        <span className={style["navbar-item-sym"]}>#</span>home
+            <div className={[style["navbar-inner"]].join(" ")}>
+                <span className={style["navbar-logo"]}>
+                    <Link href={"/"} className={style["bar-logo"]}>
+                        KawshikBss
                     </Link>
-                </li>
-                <li className={style["navbar-item-link"]}>
-                    <Link href={"/"}>
-                        <span className={style["navbar-item-sym"]}>#</span>about
-                        me
-                    </Link>
-                </li>
-                <li className={style["navbar-item-link"]}>
-                    <Link href={"/"}>
-                        <span className={style["navbar-item-sym"]}>#</span>works
-                    </Link>
-                </li>
-                <li className={style["navbar-item-link"]}>
-                    <Link href={"/"}>
-                        <span className={style["navbar-item-sym"]}>#</span>
-                        contact me
-                    </Link>
-                </li>
-            </ul>
+                </span>
+                <ul className={style["navbar-items"]}>
+                    <li className={style["navbar-item-link"]}>
+                        <Link href={"/"}>
+                            <span className={style["navbar-item-sym"]}>#</span>
+                            home
+                        </Link>
+                    </li>
+                    <li className={style["navbar-item-link"]}>
+                        <Link href={"/"}>
+                            <span className={style["navbar-item-sym"]}>#</span>
+                            about me
+                        </Link>
+                    </li>
+                    <li className={style["navbar-item-link"]}>
+                        <Link href={"/"}>
+                            <span className={style["navbar-item-sym"]}>#</span>
+                            works
+                        </Link>
+                    </li>
+                    <li className={style["navbar-item-link"]}>
+                        <Link href={"/"}>
+                            <span className={style["navbar-item-sym"]}>#</span>
+                            contact me
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
