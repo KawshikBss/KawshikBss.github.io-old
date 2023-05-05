15eaa0d40081a45e4ce4dda286ff3bd5ecad3ba8
KawshikBss <kawshikbss@gmail.com>
2023-05-05 18:27:35 +0600

    stored user data

---
+++ src/consts/profile.js
@@ -0,0 +1,24 @@
+const profile = {
+    name: 'Kawshik Biswas',
+    image: 'npp.png',
+    nickName: 'Zilch',
+    jobTitle: 'Full-Stack Developer',
+    jobDesc: 'He develops complex Web and Mobile applications',
+    currentWork: 'Currently Working On Evolutionary Tech',
+    quotes: [
+        {
+            author: 'Itachi Uchiha',
+            quote: 'In Order To Survive, We Cling To All We Know And Understand.'
+        },
+    ],
+    about: 'Officia adipisicing reprehenderit elit laboris sit esse quis pariatur eu elit nisi dolore eu veniam. Occaecat minim ex aliquip eiusmod tempor eiusmod esse qui sit elit incididunt est do qui. Cupidatat non nostrud irure irure nulla Lorem magna. Proident sint esse cillum minim aute sunt. Dolore deserunt pariatur est ad pariatur aliquip Lorem culpa et amet. Laborum aute duis adipisicing ea sunt voluptate cillum Lorem.',
+    aboutImg: 'about.JPG',
+    contacts: {
+        mail: 'kawshikbss@gmail.com',
+        github: '',
+        linkedin: '',
+        
+    }
+}
+
+export default profile;
---
+++ src/consts/skills.js
@@ -0,0 +1,35 @@
+const skills = [
+    {
+        type: 'Languages',
+        sets: [
+            'JavaScript',
+            'Python',
+            'PHP',
+            'C#',
+            'C++',
+            'C',
+            'Assembly'
+        ]
+    },
+    {
+        type: 'Frakeworks & Libraries',
+        sets: [
+            'React',
+            'Next',
+            'React-Native',
+            'Laravel',
+            'Django',
+            'Flask',
+            'Pygame',
+            'BootStrap',
+            'Tailwind',
+        ]
+    },
+    {
+        type: 'Languages',
+        sets: [
+        ]
+    },
+]
+
+export default skills;
