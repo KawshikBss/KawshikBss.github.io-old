4694835755aff149dfebc6e03472cd25b044ddc0
KawshikBss <kawshikbss@gmail.com>
2023-05-07 00:43:04 +0600

    added section heading

--- src/sections/about/About.js
+++ src/sections/about/About.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
 import React from "react";
 
 function About() {
-    return <div className="">About</div>;
+    return (
+        <div className="section-container">
+            <div className="section-heading">
+                <div className="section-heading-inner">
+                    <h5 className="section-heading-inner-text">
+                        <span className="section-heading-inner-sym">#</span>
+                        about-me
+                    </h5>
+                    <div className="section-heading-inner-line" />
+                </div>
+                <Link href={"/"} className="section-heading-btn">
+                    View All
+                </Link>
+            </div>
+        </div>
+    );
 }
 
 export default About;
