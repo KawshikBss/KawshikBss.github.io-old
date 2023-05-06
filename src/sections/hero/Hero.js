3c3780295aaa08d126a71e0de8e515ff1b1c8a78
KawshikBss <kawshikbss@gmail.com>
2023-05-06 16:40:00 +0600

    added style module

--- src/sections/hero/Hero.js
+++ src/sections/hero/Hero.js
@@ -2,28 +2,31 @@ import profile from "@/consts/profile";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import style from "./hero.module.css";
 
 function Hero() {
     return (
-        <div className="hero-container">
-            <div className="hero-info">
-                <h3 className="hero-info-heading">
+        <div className={style["hero-container"]}>
+            <div className={style["hero-info"]}>
+                <h3 className={style["hero-info-heading"]}>
                     {profile.name} is a {profile.jobTitle}
                 </h3>
-                <p className="hero-info-desc">{profile.jobDesc}</p>
+                <p className={style["hero-info-desc"]}>{profile.jobDesc}</p>
                 <Link href={"/#contact"}>
-                    <span className="hero-contact-btn">Contact Me</span>
+                    <span className={style["hero-contact-btn"]}>
+                        Contact Me
+                    </span>
                 </Link>
             </div>
-            <div className="hero-images">
+            <div className={style["hero-images"]}>
                 <Image
                     src={"npp.png"}
                     width={100}
                     height={100}
-                    className="hero-image"
+                    className={style["hero-image"]}
                 />
-                <span className="hero-quote">
-                    <span className="hero-quote-ind" />
+                <span className={style["hero-quote"]}>
+                    <span className={style["hero-quote-ind"]} />
                     {profile.currentWork}
                 </span>
             </div>
