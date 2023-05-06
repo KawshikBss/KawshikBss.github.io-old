3036b15e54053df2522a47f9fd1c11f202e3933b
KawshikBss <kawshikbss@gmail.com>
2023-05-06 18:16:46 +0600

    added hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
+import Hero from "@/sections/hero/Hero";
+
 export default function Home() {
     return (
         <>
-            <div className="section">home</div>
+            <div className="section">
+                <Hero />
+            </div>
             <div className="section">about</div>
         </>
     );
