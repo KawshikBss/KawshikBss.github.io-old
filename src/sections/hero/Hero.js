eaf2bc0463c9e929848d33decef7f1bc037bb443
KawshikBss <kawshikbss@gmail.com>
2023-05-06 18:31:56 +0600

    completed responsiveness

--- src/sections/hero/Hero.js
+++ src/sections/hero/Hero.js
@@ -6,17 +6,34 @@ import style from "./hero.module.css";
 
 function Hero() {
     return (
-        <div className={style["hero-container"]}>
-            <div className={style["hero-info"]}>
+        <div
+            className={[
+                style["hero-container"],
+                "flex-col md:flex-row w-full md:w-[65%] mt-16 md:mt-0",
+            ].join(" ")}
+        >
+            <div
+                className={[style["hero-info"], "w-9/10 md:w-[55%]"].join(" ")}
+            >
                 <h3 className={style["hero-info-heading"]}>
                     {profile.name} is a {profile.jobTitle}
                 </h3>
                 <p className={style["hero-info-desc"]}>{profile.jobDesc}</p>
-                <Link href={"/#contact"} className={style["hero-contact-btn"]}>
+                <Link
+                    href={"/#contact"}
+                    className={[
+                        style["hero-contact-btn"],
+                        "hidden md:block",
+                    ].join(" ")}
+                >
                     Contact Me
                 </Link>
             </div>
-            <div className={style["hero-images"]}>
+            <div
+                className={[style["hero-images"], "w-9/10 md:w-[55%]"].join(
+                    " "
+                )}
+            >
                 <Image
                     src={"npp.png"}
                     width={100}
