81d3c1e8ac2b0f66c13d75ec7f1663dc472daa4b
KawshikBss <kawshikbss@gmail.com>
2023-05-07 20:58:02 +0600

    completed project component

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,7 +1,46 @@
 import React from "react";
+import style from "./project.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 function Project() {
-    return <div>Project</div>;
+    return (
+        <div className={style["project-container"]}>
+            <Link href={"/"} className={style["project-thumbnail-container"]}>
+                <Image
+                    src={"project1.png"}
+                    height={100}
+                    width={100}
+                    className={style["project-thumbnail"]}
+                />
+            </Link>
+            <span className={style["project-tech-list"]}>
+                <span className={style["project-tech-list-item"]}>HTML</span>
+                <span className={style["project-tech-list-item"]}>HTML</span>
+                <span className={style["project-tech-list-item"]}>HTML</span>
+                <span className={style["project-tech-list-item"]}>HTML</span>
+                <span className={style["project-tech-list-item"]}>HTML</span>
+                <span className={style["project-tech-list-item"]}>HTML</span>
+                <span className={style["project-tech-list-item"]}>HTML</span>
+                <span className={style["project-tech-list-item"]}>HTML</span>
+                <span className={style["project-tech-list-item"]}>HTML</span>
+                <span className={style["project-tech-list-item"]}>HTML</span>
+                <span className={style["project-tech-list-item"]}>HTML</span>
+                <span className={style["project-tech-list-item"]}>HTML</span>
+            </span>
+            <div className={style["project-info"]}>
+                <h3 className={style["project-info-title"]}>
+                    Alvita Nutrition
+                </h3>
+                <p className={style["project-info-desc"]}>
+                    Magna reprehenderit adipisicing.
+                </p>
+                <Link href={"/"} className="button">
+                    Live
+                </Link>
+            </div>
+        </div>
+    );
 }
 
 export default Project;
