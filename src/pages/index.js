1c9950691a363a01da5a5379c18f45ca94203ee6
KawshikBss <kawshikbss@gmail.com>
2023-05-07 15:17:47 +0600

    added projects section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import About from "@/sections/about/About";
 import Hero from "@/sections/hero/Hero";
+import Projects from "@/sections/projects/Projects";
 
 export default function Home() {
     return (
@@ -9,6 +10,9 @@ export default function Home() {
             </div>
             <div className="section">
                 <About />
+            </div>
+            <div className="section">
+                <Projects />
             </div>
         </>
     );
