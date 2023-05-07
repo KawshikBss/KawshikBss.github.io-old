a868f08978ced0077fb3f9474bcc394886a98f56
KawshikBss <kawshikbss@gmail.com>
2023-05-07 15:18:03 +0600

    added section container and heading

--- src/sections/projects/Projects.js
+++ src/sections/projects/Projects.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Projects() {
-  return (
-    <div>Projects</div>
-  )
+    return (
+        <div className="section-container">
+            <div className="section-heading">
+                <div className="section-heading-inner">
+                    <h5 className="section-heading-inner-text">
+                        <span className="section-heading-inner-sym">#</span>
+                        my-works
+                    </h5>
+                    <div className="section-heading-inner-line" />
+                </div>
+                <Link href={"/"} className="section-heading-btn">
+                    View All
+                </Link>
+            </div>
+        </div>
+    );
 }
 
-export default Projects
+export default Projects;
