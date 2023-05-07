ae0ae34dba3e970fb6e6dcfbbb125c1df3dd923f
KawshikBss <kawshikbss@gmail.com>
2023-05-08 02:08:32 +0600

    completed functionality

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./carousel.module.css";
-import { AiFillCaretRight } from "react-icons/ai";
+import { AiFillCaretLeft } from "react-icons/ai";
 
 function Carousel({ children }) {
+    const [currentPos, setCurrentPos] = useState(0);
+    const xOffset = 90;
+    const next = () => {
+        setCurrentPos((curr) => {
+            if (curr + 1 >= children.length) {
+                return 0;
+            }
+            return curr + 1;
+        });
+    };
     return (
         <div className={style["carousel-container"]}>
-            <div className={style["carousel-items"]}>{children}</div>
-            <span className={style["carousel-btn"]}>
-                <AiFillCaretRight />
+            <div
+                className={style["carousel-items"]}
+                style={{ right: `-${currentPos * xOffset}vw` }}
+            >
+                {children}
+            </div>
+            <span className={style["carousel-btn"]} onClick={next}>
+                <AiFillCaretLeft className={style["carousel-btn-inner"]} />
             </span>
         </div>
     );
