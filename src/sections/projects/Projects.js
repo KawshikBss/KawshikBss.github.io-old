2056cb44fb666a7f9ff5fae377cdbb60346766b7
KawshikBss <kawshikbss@gmail.com>
2023-05-07 20:58:34 +0600

    added projects in project list

--- src/sections/projects/Projects.js
+++ src/sections/projects/Projects.js
@@ -1,4 +1,7 @@
+import Link from "next/link";
 import React from "react";
+import style from "./projects.module.css";
+import Project from "@/components/project/Project";
 
 function Projects() {
     return (
@@ -7,13 +10,18 @@ function Projects() {
                 <div className="section-heading-inner">
                     <h5 className="section-heading-inner-text">
                         <span className="section-heading-inner-sym">#</span>
-                        my-works
+                        projects
                     </h5>
                     <div className="section-heading-inner-line" />
                 </div>
                 <Link href={"/"} className="section-heading-btn">
                     View All
                 </Link>
+            </div>
+            <div className={style["projects-inner"]}>
+                <Project />
+                <Project />
+                <Project />
             </div>
         </div>
     );
