dda5ffd5c0f3002e4344b0b3a7d4a07e172b872a
KawshikBss <kawshikbss@gmail.com>
2023-05-08 18:01:36 +0600

    completed markup

--- src/sections/skills/Skills.js
+++ src/sections/skills/Skills.js
@@ -1,4 +1,6 @@
 import React from "react";
+import style from "./skills.module.css";
+import Image from "next/image";
 
 function Skills() {
     return (
@@ -10,6 +12,106 @@ function Skills() {
                         skills
                     </h5>
                     <div className="section-heading-inner-line" />
+                </div>
+            </div>
+            <div className={style["skills-inner"]}>
+                <Image
+                    src={"skillsbg.png"}
+                    width={100}
+                    height={100}
+                    className={style["skills-bg"]}
+                    alt="skillsbg"
+                />
+                <div className={style["skills-list"]}>
+                    <div className={style["skills-list-col"]}>
+                        <div className={style["skills-list-item"]}>
+                            <span className={style["skill-category"]}>
+                                Languages
+                            </span>
+                            <div className={style["skill-sets"]}>
+                                <span className={style["skill-set-item"]}>
+                                    excepteur
+                                </span>
+                                <span className={style["skill-set-item"]}>
+                                    excepteur
+                                </span>
+                                <span className={style["skill-set-item"]}>
+                                    excepteur
+                                </span>
+                                <span className={style["skill-set-item"]}>
+                                    excepteur
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={style["skills-list-col"]}>
+                        <div className={style["skills-list-item"]}>
+                            <span className={style["skill-category"]}>
+                                Languages
+                            </span>
+                            <div className={style["skill-sets"]}>
+                                <span className={style["skill-set-item"]}>
+                                    Consectetur
+                                </span>
+                                <span className={style["skill-set-item"]}>
+                                    Consectetur
+                                </span>
+                                <span className={style["skill-set-item"]}>
+                                    Consectetur
+                                </span>
+                            </div>
+                        </div>
+                        <div className={style["skills-list-item"]}>
+                            <span className={style["skill-category"]}>
+                                Languages
+                            </span>
+                            <div className={style["skill-sets"]}>
+                                <span className={style["skill-set-item"]}>
+                                    Lorem
+                                </span>
+                                <span className={style["skill-set-item"]}>
+                                    Lorem
+                                </span>
+                                <span className={style["skill-set-item"]}>
+                                    Lorem
+                                </span>
+                                <span className={style["skill-set-item"]}>
+                                    Lorem
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={style["skills-list-col"]}>
+                        <div className={style["skills-list-item"]}>
+                            <span className={style["skill-category"]}>
+                                Languages
+                            </span>
+                            <div className={style["skill-sets"]}>
+                                <span className={style["skill-set-item"]}>
+                                    Laborum
+                                </span>
+                                <span className={style["skill-set-item"]}>
+                                    Laborum
+                                </span>
+                            </div>
+                        </div>
+                        <div className={style["skills-list-item"]}>
+                            <span className={style["skill-category"]}>
+                                Languages
+                            </span>
+                            <div className={style["skill-sets"]}>
+                                <span className={style["skill-set-item"]}>
+                                    Officia
+                                </span>
+                                <span className={style["skill-set-item"]}>
+                                    Officia
+                                </span>
+                                <span className={style["skill-set-item"]}>
+                                    Officia
+                                </span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
