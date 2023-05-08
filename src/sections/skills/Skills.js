3a0123dbd668276c1bd1ba0162e4062221cb3ed8
KawshikBss <kawshikbss@gmail.com>
2023-05-08 14:39:05 +0600

    added container and heading

--- src/sections/skills/Skills.js
+++ src/sections/skills/Skills.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Skills() {
-    return <div>Skills</div>;
+    return (
+        <div className="section-container">
+            <div className="section-heading">
+                <div className="section-heading-inner">
+                    <h5 className="section-heading-inner-text">
+                        <span className="section-heading-inner-sym">#</span>
+                        skills
+                    </h5>
+                    <div className="section-heading-inner-line" />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Skills;
