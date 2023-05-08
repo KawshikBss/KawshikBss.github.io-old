a60165882f5a305b9cd730333597ed1d9dc7bd9e
KawshikBss <kawshikbss@gmail.com>
2023-05-08 18:05:34 +0600

    added section container and heading

--- src/sections/contacts/Contacts.js
+++ src/sections/contacts/Contacts.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Contacts() {
-    return <div>Contacts</div>;
+    return (
+        <div className="section-container">
+            <div className="section-heading">
+                <div className="section-heading-inner">
+                    <h5 className="section-heading-inner-text">
+                        <span className="section-heading-inner-sym">#</span>
+                        contacts
+                    </h5>
+                    <div className="section-heading-inner-line" />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Contacts;
