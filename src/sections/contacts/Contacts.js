94549433e0384abf2bde494431142179e1aaf24d
KawshikBss <kawshikbss@gmail.com>
2023-05-08 18:20:42 +0600

    completed markup

--- src/sections/contacts/Contacts.js
+++ src/sections/contacts/Contacts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import style from "./contacts.module.css";
 
 function Contacts() {
     return (
@@ -10,6 +11,32 @@ function Contacts() {
                         contacts
                     </h5>
                     <div className="section-heading-inner-line" />
+                </div>
+            </div>
+            <div className={style["contacts-inner"]}>
+                <div className={style["contacts-desc"]}>
+                    Adipisicing ad mollit voluptate laboris aute velit amet non
+                    nostrud id tempor dolore aliquip voluptate. Sit in in
+                    laboris ut Lorem deserunt irure officia culpa laborum.
+                    Mollit eiusmod eiusmod ipsum dolor. Proident anim aliqua qui
+                    occaecat dolor dolor culpa Lorem ea cupidatat. Aliqua magna
+                    sit cillum ea cillum dolore ipsum esse elit excepteur sunt
+                    proident nisi. Excepteur esse deserunt magna voluptate
+                    exercitation occaecat dolor proident ea nisi officia do.
+                </div>
+                <div className={style["contacts-list"]}>
+                    <span className={style["contacts-list-heading"]}>
+                        Message Me Here
+                    </span>
+                    <span className={style["contacts-list-item"]}>
+                        kawshikbss@gmail.com
+                    </span>
+                    <span className={style["contacts-list-item"]}>
+                        kawshikbss@gmail.com
+                    </span>
+                    <span className={style["contacts-list-item"]}>
+                        kawshikbss@gmail.com
+                    </span>
                 </div>
             </div>
         </div>
