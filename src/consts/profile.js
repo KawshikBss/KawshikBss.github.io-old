6dd15610a2589e63c1d2d13ef3fbb07cb35fdfdb
KawshikBss <kawshikbss@gmail.com>
2023-06-13 02:22:59 +0600

    modified info

--- src/consts/profile.js
+++ src/consts/profile.js
@@ -1,24 +1,18 @@
 const profile = {
-    name: 'Kawshik Biswas',
-    image: 'npp.png',
-    nickName: 'Zilch',
-    jobTitle: 'Full-Stack Developer',
-    jobDesc: 'He develops complex Web and Mobile applications',
-    currentWork: 'I am an artist weather the canvas is a piece of paper or a block of code',
-    quotes: [
-        {
-            author: 'Itachi Uchiha',
-            quote: 'In Order To Survive, We Cling To All We Know And Understand.'
-        },
-    ],
-    about: 'Officia adipisicing reprehenderit elit laboris sit esse quis pariatur eu elit nisi dolore eu veniam. Occaecat minim ex aliquip eiusmod tempor eiusmod esse qui sit elit incididunt est do qui. Cupidatat non nostrud irure irure nulla Lorem magna. Proident sint esse cillum minim aute sunt. Dolore deserunt pariatur est ad pariatur aliquip Lorem culpa et amet. Laborum aute duis adipisicing ea sunt voluptate cillum Lorem.',
-    aboutImg: 'about.JPG',
+    name: "Kawshik Biswas",
+    image: "npp.png",
+    nickName: "Zilch",
+    jobTitle: "Full-Stack Developer",
+    jobDesc: "He develops complex Web and Mobile applications",
+    currentWork:
+        "I am an artist whether the canvas is a piece of paper or a block of code",
+    about: "As a Full Stack Developer with almost 3 years of experience in web development and 1 year of experience in mobile app development, I have a passion for creating innovative solutions that meet user needs. My key skills include ReactJS, React Native, NextJS, ExpressJS, NodeJS, and Git, and I have a strong understanding of API and SOLID programming concepts. I am committed to lifelong learning and keeping up with the latest trends in technology. I approach every project with a can-do attitude and firmly believe that if it was developed by someone else, it can be developed again - and if it was never tried, I'm willing to take on the challenge.",
+    aboutImg: "about.JPG",
     contacts: {
-        mail: 'kawshikbss@gmail.com',
-        github: '',
-        linkedin: '',
-        
-    }
-}
+        mail: "kawshikbss@gmail.com",
+        github: "",
+        linkedin: "",
+    },
+};
 
 export default profile;
