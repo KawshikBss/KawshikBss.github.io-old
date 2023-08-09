9f45b414854d3c3504908e65414a2c1e3a191653
KawshikBss <kawshikbss@gmail.com>
2023-08-10 02:02:21 +0600

    modified responsive contents

--- src/sections/contacts/Contacts.js
+++ src/sections/contacts/Contacts.js
@@ -21,11 +21,6 @@ function Contacts() {
                     Adipisicing ad mollit voluptate laboris aute velit amet non
                     nostrud id tempor dolore aliquip voluptate. Sit in in
                     laboris ut Lorem deserunt irure officia culpa laborum.
-                    Mollit eiusmod eiusmod ipsum dolor. Proident anim aliqua qui
-                    occaecat dolor dolor culpa Lorem ea cupidatat. Aliqua magna
-                    sit cillum ea cillum dolore ipsum esse elit excepteur sunt
-                    proident nisi. Excepteur esse deserunt magna voluptate
-                    exercitation occaecat dolor proident ea nisi officia do.
                 </div>
                 <div className={style["contacts-list"]}>
                     <span className={style["contacts-list-heading"]}>
